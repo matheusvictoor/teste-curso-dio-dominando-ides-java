6f40201f9c211c6e5b571b063e73ec1040e5c9f3
Matheus Victor <matheusvictor.dev@gmail.com>
2022-06-28 08:50:46 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato("jack","amarelo", 8);
+		
+		
+		System.out.println(gato);
+	}
+
+}
